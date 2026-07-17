5de922787abe717a27909a3693bf685192d5c41b
RAIZEY5010 <udjej236@gmail.com>
2026-07-17 12:14:06 +0200

    Add light class to HTML element

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ar" dir="rtl">
+    <html lang="ar" dir="rtl" className="light">
       <body className="antialiased bg-background text-foreground">
         {children}
         {process.env.NODE_ENV === 'production' && <Analytics />}
